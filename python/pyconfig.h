4d2bcbbb9b501e886d29d83ba9a9bebf72f99836
Lindell Alderman <admin@darkmatter.io>
2012-07-14 23:27:55 -0700

    opensuse builds through libxml2/libxslt

--- python/pyconfig.h
+++ python/pyconfig.h
@@ -953,7 +953,7 @@
 /* #undef Py_DEBUG */
 
 /* Defined if Python is built as a shared library. */
-#define Py_ENABLE_SHARED 1
+/* #undef Py_ENABLE_SHARED */
 
 /* Define as the size of the unicode type. */
 #define Py_UNICODE_SIZE 2
